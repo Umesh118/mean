4c67c400c3017953f4655b76ca19b1521db190bb
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-10-28 15:53:31 +0530

    Initial

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var anime: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: '.c2 .word',
+        scale: [14, 1],
+        opacity: [0, 1],
+        easing: 'easeOutCirc',
+        duration: 800,
+
+        // delay: (el: any, i: any) => 800 * i,
+      })
+      .add({
+        targets: '.c2',
+        opacity: 0,
+        duration: 1500,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      });
   }
-
 }
